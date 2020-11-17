1b6df43f2d278afdd018504f256bad3133bcd0fc
concept10 <tenthconcept@gmail.com>
2020-11-17 13:36:58 -0600

    Modified index.ts before editor change.

---
+++ documentation/notes.md
@@ -0,0 +1,11 @@
+
+
+system requirements:
+
+dependencies:
+
+licences encountered:
+
+
+other:
+Should have used oclif's CLI generator to begin this scaffolding.
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+import * as fs from 'fs';
+import { Command, flags } from '@oclif/command';
 
-import { Command } from '@oclif/command';
+export class LS extends Command {
 
-export class SomeCommand extends Command { 
+	static flags = {
+		version: flags.version(),
+		help: flags.help()
+	}
 	async run() {
+		const {flags} = this.parse(LS)
+		let files = fs.readdirSync(flags.dir)
+		for (let f of files) {}
+		
 		console.log('Console output from SomeCommand!');
 	};
 };
